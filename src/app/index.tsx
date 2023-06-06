ed274a810f051bc2a21e148bb29126795549586d
rtr <rtr@vt2b.ru>
2023-06-06 11:53:21 +0500

    normalize

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
-import './index.scss';
+import { type FC } from "react"
+import "./index.scss"
+import { BaseLayout } from "shared/layout"
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+const App: FC = () => {
+    return (
+        <BaseLayout/>
+    )
 }
 
-export default App;
+export default App
---
+++ src/shared/layout/BaseLayout/index.tsx
@@ -0,0 +1,12 @@
+import { type FC } from "react"
+import "./index.scss"
+
+const BaseLayout: FC = () => {
+    return (
+        <section className="base-layout">
+            Hello
+        </section>
+    )
+}
+
+export default BaseLayout
